34ad4ce8e91824fb7a47ed31a40b28d60af81892
o9uzdev <o9uzdev@gmail.com>
2021-06-25 10:45:55 +0300

    Added button event

--- src/com/o9uzdev/javaFxRandomColorChanger/App.java
+++ src/com/o9uzdev/javaFxRandomColorChanger/App.java
@@ -1,12 +1,15 @@
 package com.o9uzdev.javaFxRandomColorChanger;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -21,6 +24,17 @@ public class App extends Application {
         label.setPadding(new Insets(50));
 
         Button button = new Button("Random Color");
+
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+             int red = (int) (Math.random()*256);
+             int green = (int) (Math.random()*256);
+             int blue = (int) (Math.random()*256);
+             Color customColor = Color.rgb(red,green,blue);
+                label.setTextFill(customColor);
+         }
+     });
 
         root.getChildren().addAll(label, button);
 
